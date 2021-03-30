f2e9073073b4d446579a941b0dfd6506af52a7b3
largemable <mable.palombo@gmail.com>
2021-03-30 12:36:51 -0500

    Create form
    Co-authored by Stephane stephane.karim.sk@gmail.com
    Co-Authored by Jordan mrjordantanner@gmail.com
    Co-Authored by Ford fordpickert@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 // import { useState, useEffect } from 'react'
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 import './App.css';
-import Albums from './components/Albums'
+import Albums from './components/Albums';
+import Form from './components/CreatePost';
 // import Nav from './components/Nav'
 // import APIurl from './config'
 // import axios from 'axios'
 
 function App() {
+	// useEffect(() => {
+	//   axios
+	//   .get(`${APIurl}/albums`)
+	//   .then((res) => setAlbums(res.data))
+	//   .catch(console.error);
+	// }, [])
 
-  // useEffect(() => {
-  //   axios
-  //   .get(`${APIurl}/albums`)
-  //   .then((res) => setAlbums(res.data))
-  //   .catch(console.error);
-  // }, [])
+	return (
+		<div>
+			{/* <Nav/> */}
+			{/* <Route path="/" exact component={ <Albums /> }/> */}
 
-  return (
-    <div>
-        {/* <Nav/> */}
-        {/* <Route path="/" exact component={ <Albums /> }/> */}
-
-      {/* Displays all albums  */}
-      <Albums />  
-
-    </div>
-  );
+			{/* Displays all albums  */}
+			<Albums />
+			<Form form={Form} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/CreatePost.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import APIurl from '../config';
+
+const Form = () => {
+	const initialState = {
+		title: '',
+		artist: '',
+		artwork: '',
+		genre: '',
+		year: '',
+	};
+
+	const history = useHistory();
+	const [album, setAlbum] = useState(initialState);
+
+	const handleChange = (event) => {
+		// event.preventDefault();
+		setAlbum({ ...album, [event.target.name]: event.target.value });
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Write your POST fetch() or axios() request here
+		axios
+			.post(`${APIurl}/albums`, album)
+			.then(() => {
+				history.push('/');
+			})
+			.catch(console.error);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit} className='create-form'>
+				<label htmlFor='title'>Album Title: </label>
+				<input
+					onChange={handleChange}
+					name='title'
+					value={album.title}
+					placeholder='Album'
+				/>
+				<label htmlFor='artist'>Artist: </label>
+				<input
+					onChange={handleChange}
+					name='artist'
+					value={album.artist}
+					placeholder='Artist'
+				/>
+				<label htmlFor='artwork'>Artwork: </label>
+				<input
+					onChange={handleChange}
+					name='artwork'
+					value={album.artwork}
+					placeholder='Artwork'
+				/>
+				<label htmlFor='genre'>Genre: </label>
+				<input
+					onChange={handleChange}
+					name='genre'
+					value={album.genre}
+					placeholder='Genre'
+				/>
+				<label htmlFor='year'>Release Year: </label>
+				<input
+					onChange={handleChange}
+					name='year'
+					value={album.year}
+					placeholder='Year'
+				/>
+				<button id='button' type='submit'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Form;
