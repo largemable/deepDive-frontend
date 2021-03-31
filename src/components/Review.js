bd89552f46475769c39e5ce519a18fbcac495b65
largemable <mable.palombo@gmail.com>
2021-03-31 12:18:12 -0500

    create delete handler
    Co-Authored by Ford fordpickert@gmail.com
    Co-Authored by Stephane stephane.karim.sk@gmail.com
    Co-Authored by Jordan mrjordantanner@gmail.com

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
 import '../App.css';
+import APIurl from '../config';
+import axios from 'axios';
 
-export default function Review( { review } ) {
-    return (
-        <div className='review'>
-            <h1>{`Review Title: ${review.title}`}</h1>
-            <p>{`${review.body}`}</p>
-        </div>
-    )
+export default function Review({ review, id, setAlbum }) {
+	const handleDelete = () => {
+		// Write your DELETE fetch() or axios() request here
+
+		axios
+			.delete(`${APIurl}/reviews/${id}`, review)
+			.then(() => {
+				// history.push('/');
+			})
+			.catch(console.error);
+	};
+	return (
+		<div className='review'>
+			<h2>{`${review.title}`}</h2>
+			<p>{`${review.body}`}</p>
+			<div className='controls'>
+				<button onClick={handleDelete}>Delete</button>
+				{/* <button onClick={handleEdit}>Edit</button> */}
+			</div>
+		</div>
+	);
 }
