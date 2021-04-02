501e862ee790fe136eb1b74d8df260efb58769c2
mrjordantanner <mrjordantanner@gmail.com>
2021-04-02 12:28:42 -0500

    Update axios request to require user token

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const APIurl =
-	'https://mongodb+srv:/caffeine-dreams:k6l3LXnOIc62moON@cluster0.0kym9.mongodb.net/music-review-app?retryWrites=true&w=majority';
+// const APIurl = 'http://localhost:4000';
+const APIurl = 'https://music-review-dreams.herokuapp.com';
 
 export default APIurl;
