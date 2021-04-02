879d4eacf485643aa00c8bedeeac53cf1be62534
mrjordantanner <mrjordantanner@gmail.com>
2021-04-02 14:04:16 -0500

    Require Patch and Delete requests to require user token

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+// import SimpleRating from './SimpleRating';
+import Rating from '@material-ui/lab/Rating';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function Home() {
     return (
@@ -7,6 +10,7 @@ export default function Home() {
             <h1>Music Blog and Rating App</h1>
             <p>Post, review, and rate your favorite albums!</p>
             <Link className='ghost-button' to='/albums'>Get Started</Link>
+            <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
         </div>
     )
 }
